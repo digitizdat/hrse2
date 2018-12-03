4e9a9bbdba452ec842c5c7b986e7b14263500fa9
Martin McGreal <martin.mcgreal@ehi.com>
2018-12-02 23:11:53 -0600

    Converted to Gin web framework

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "log"
-  "net/http"
+    "net/http"
+
+    "github.com/gin-gonic/gin"
+    "github.com/gin-gonic/contrib/static"
+    "os"
 )
 
 func main() {
-  addr := ":"+os.Getenv("PORT")
-  http.HandleFunc("/", hello)
-  log.Fatal(http.ListenAndServe(addr, nil))
+    port := ":" + os.Getenv("PORT")
+
+    r := gin.Default()
+    r.Use(static.Serve("/", static.LocalFile("./public", true)))
+    r.Use(static.Serve("/favicon.ico", static.LocalFile("/img/hrse.ico", true)))
+
+    r.GET("/", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "title": "HRSE",
+        })
+    })
+    r.GET("/ping", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "message": "pong",
+        })
+    })
+
+    r.Run(port)
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "Hello World from Go")
-}
